446b517d4b0cd5c835784b5a59b70dc7703a2a7d
hackerhucker <fatihozkul3541@hotmail.com>
2022-01-25 14:14:03 +0100

    small changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const button2 = document.querySelector(".btn-secondary");
 
-button2.addEventListener("click", setPosition);
+button2.addEventListener("mouseover", setPosition);
 
 let randomPositionListX = ["12%", "43%", "88%"];
 let randomPositionListY = ["35%", "70%", "85%"];
@@ -9,7 +9,6 @@ function setPosition() {
   button2.style.left = randomPositionListX[randomPositionX()];
 
   button2.style.top = randomPositionListY[randomPositionY()];
-  console.log(button2.style.left, button2.style.top);
 }
 
 function randomPositionX() {
